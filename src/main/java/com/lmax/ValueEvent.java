7187e846c44ae4dc51573b838b19403b9f927dd4
Michael Barker <mikeb01@gmail.com>
2015-12-10 19:29:39 +1300

    Initial Disruptor JMH benchmark.

---
+++ src/main/java/com/lmax/ValueEvent.java
@@ -0,0 +1,26 @@
+package com.lmax;
+
+import com.lmax.disruptor.EventFactory;
+
+public final class ValueEvent
+{
+    private long value;
+
+    public long getValue()
+    {
+        return value;
+    }
+
+    public void setValue(final long value)
+    {
+        this.value = value;
+    }
+
+    public static final EventFactory<ValueEvent> EVENT_FACTORY = new EventFactory<ValueEvent>()
+    {
+        public ValueEvent newInstance()
+        {
+            return new ValueEvent();
+        }
+    };
+}
